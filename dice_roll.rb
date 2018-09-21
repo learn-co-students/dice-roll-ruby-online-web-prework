d9f30bbf9aab5cf4ce6f980b7c79d08da3257d35
Brandon Teich <brandonteich123@gmail.com>
2018-09-21 00:16:56 +0000

    automatically backed up by learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,7 +3,6 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  Random.new_seed  #=> 115032730400174366788466674494640623225
-  puts rand(0...6)
-  rand(0...6)
+  Random.new_seed
+  
 end
