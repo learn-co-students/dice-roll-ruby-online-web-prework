58753afce36b381cfcfaebc16668c1a3c96de6e5
Sam Bennrubi <sbennrubi@gmail.com>
2018-10-28 16:16:50 +0000

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,6 +3,6 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  myArray = [1,2,3,4,5,6]
-  myArray[rand(myArray.length)]
+  diceArray = [1,2,3,4,5,6]
+  diceArray[rand(diceArray.length)]
 end
