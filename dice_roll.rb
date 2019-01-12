8f4c120fd1eaddb67425f106dcdf8844df845a17
Mark Kanko <mkanko66@gmail.com>
2019-01-12 21:33:00 +0000

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,6 +2,13 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
+# def roll
+  # lower_limit = 1 
+  # upper_limit = 6 
+  # Random.new.rand(lower_limit..upper_limit)
+# end
+
 def roll
-  # code goes here
+  r = [1,2,3,4,5,6]
+  rand(1..6)
 end
